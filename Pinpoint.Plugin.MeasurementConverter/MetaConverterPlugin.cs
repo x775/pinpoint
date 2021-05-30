568020accda54d1b45ecd6bebc79fb0422658d7d
x775 <x775@users.noreply.github.com>
2021-05-30 23:07:30 +0200

    ... attempt at reflection

--- Pinpoint.Plugin.MeasurementConverter/MetaConverterPlugin.cs
+++ Pinpoint.Plugin.MeasurementConverter/MetaConverterPlugin.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Pinpoint.Core;
 using Pinpoint.Core.Results;
-using Pinpoint.Plugin.MetaConverter.Converters;
 
 namespace Pinpoint.Plugin.MetaConverter
 {
@@ -13,10 +14,7 @@ namespace Pinpoint.Plugin.MetaConverter
         // Match left- and right-hand side of any conversion.
         private static readonly Regex Pattern = new Regex(@"^(\d+) ?(\w*){1}( (to|in) )?(\w*)?");
         private static Match _match;
-
-        var types = typeof(MetaConverterPlugin).Assembly.GetTypes();
-        private static Type[] typeCandidates = types.Where(t => t.IsClass && t.GetInterface(nameof(IConverter)) != null).ToArray();
-
+        
 
         public PluginMeta Meta { get; set; } = new PluginMeta("Meta Converter", PluginPriority.Highest);
         public PluginSettings UserSettings { get; set; } = new PluginSettings();
@@ -26,21 +24,43 @@ namespace Pinpoint.Plugin.MetaConverter
         {
         }
 
+        private static Type[] typeCandidates = default;
+
+        private static double? MatchAndConvert(string fromString, string toString, double value)
+        {
+            foreach (var type in typeCandidates)
+            {
+                var converter = (IConverter)Activator.CreateInstance(type)!;
+                if (Enum.TryParse(converter.Unit, fromString.ToLower(), out var fromEnum))
+                {
+                    if (Enum.TryParse(converter.Unit, toString.ToLower(), out var toEnum))
+                    {
+                        ((IConverter)Activator.CreateInstance(type)!)!.Convert((Enum)fromEnum!, (Enum)toEnum!, value);
+                    }
+                }
+            }
+
+            return null;
+        }
+
         private static Tuple<string, double> ConvertQuery(Query query)
         {
+            var types = typeof(MetaConverterPlugin).Assembly.GetTypes();
+            typeCandidates = types.Where(t => t.IsClass && t.GetInterface(nameof(IConverter)) != null).ToArray();
+
             // match.Groups[0].Value holds the entire matched expression.
-            var value = _match.Groups[1].Value;
+            var value = double.Parse(_match.Groups[1].Value, CultureInfo.InvariantCulture.NumberFormat);
             var fromUnit = _match.Groups[2].Value;
             var toUnit = _match.Groups[5].Value;
 
-            // Handle non specified toUnit.
-            if (_match.Groups[3].Value == "" || toUnit == "")
+            var result = MatchAndConvert(fromUnit, toUnit, value);
+
+            if (result != null)
             {
-                toUnit = "m";
+                return new Tuple<string, double>(toUnit, Math.Round((double)result, 5));
             }
 
-            var conversion = UnitConverter.ConvertTo(fromUnit, toUnit, Convert.ToDouble(value));
-            return new Tuple<string, double>(toUnit, Math.Round(conversion, 5));
+            return null;
         }
 
         public async Task<bool> Activate(Query query)
@@ -50,18 +70,6 @@ namespace Pinpoint.Plugin.MetaConverter
         }
 
         public async IAsyncEnumerable<AbstractQueryResult> Process(Query query)
-        {
-            var tuple = ConvertQuery(query);
-            yield return new ConversionResult(tuple.Item1, tuple.Item2);
-        }
-
-        public async Task<bool> Activate(Query query)
-        {
-            _match = Pattern.Match(query.RawQuery);
-            return _match != default && _match.Success;
-        }
-
-        public async IAsyncEnumerable<AbstractQueryResult> IPlugin.Process(Query query)
         {
             var tuple = ConvertQuery(query);
             yield return new ConversionResult(tuple.Item1, tuple.Item2);
